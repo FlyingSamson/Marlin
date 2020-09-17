2d14678f884b11587c1d6d5c295d1257c9829771
Dominik Vietinghoff <dominik@tuxcad.de>
2020-09-17 18:51:08 +0200

    Adapt probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1008,7 +1008,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -38, -19, -2.87 }
+#define NOZZLE_TO_PROBE_OFFSET { -38, -19, -0.38 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
