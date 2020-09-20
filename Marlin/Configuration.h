b539f2edf8a3bdb94135e86c9cc37dc76326a28f
Dominik Vietinghoff <dominik@tuxcad.de>
2020-09-20 19:19:16 +0200

    Update PID for hotends and bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -498,9 +498,9 @@
 
 
   // Flyingbear Tornado 2 Pro
-  #define DEFAULT_Kp_LIST { 14.85}
-  #define DEFAULT_Ki_LIST {  1.62}
-  #define DEFAULT_Kd_LIST { 34.06}
+  #define DEFAULT_Kp_LIST { 15.14}
+  #define DEFAULT_Ki_LIST {  1.68}
+  #define DEFAULT_Kd_LIST { 34.10}
 
   // Ultimaker
   // #define DEFAULT_Kp 22.2
@@ -552,11 +552,16 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
+  // Flyingbear Tornado 2 Pro
+  #define DEFAULT_bedKp  97.33
+  #define DEFAULT_bedKi  16.28
+  #define DEFAULT_bedKd 388.07
+
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  // #define DEFAULT_bedKp 10.00
+  // #define DEFAULT_bedKi .023
+  // #define DEFAULT_bedKd 305.4
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
